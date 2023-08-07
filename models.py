af15e3d366a94b545e72ae2008808d3710acb9fb
Preetham Tikkireddi <thspreetham@gmail.com>
2023-08-07 23:29:45 +0530

    big bang

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from qiskit import QuantumCircuit
+import uuid
+
 
 class VictimCircuit(QuantumCircuit):
     def __init__(self, *args, **kwargs):
@@ -12,3 +14,13 @@ class VictimCircuit(QuantumCircuit):
     def cx(self, ctrl, target):
         super().cx(ctrl, target)
         self.cnots_applied.append((ctrl, target))
+
+
+
+class VictimParams:
+    def __init__(self, no_of_qubits, max_circuit_depth, cnot_vals, no_of_circuits_per_cnot_val):
+        self.no_of_qubits = no_of_qubits
+        self.max_circuit_depth = max_circuit_depth
+        self.cnot_vals = cnot_vals
+        self.no_of_circuits_per_cnot_val = no_of_circuits_per_cnot_val
+        self.uuid: uuid.UUID = uuid.uuid4()
